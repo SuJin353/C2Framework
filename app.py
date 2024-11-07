26b41cc50366560bd274f7d8515eb6d35b500064
SuJin353 <phu6771@gmail.com>
2024-11-07 21:24:37 +0700

    html + css

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import resources
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_restful import Api
 from database.db import initialize_db
 
@@ -21,6 +21,23 @@ api = Api(app)
 
 # Define the routes for each of our resources
 api.add_resource(resources.Tasks, '/tasks', endpoint='tasks')
+
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/payloads')
+def payloads():
+    return render_template('payloads.html')
+
+@app.route('/listeners')
+def listeners():
+    return render_template('listeners.html')
+
+@app.route('/connections')
+def connections():
+    return render_template('connections.html')
 
 # Start the Flask app in debug mode
 if __name__ == '__main__':
